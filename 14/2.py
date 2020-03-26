c66ec01016f0e7f829e3ecbd92e28182ac455d24
xilongxin <xilongxin@weigankeji.com>
2020-03-26 16:23:10 +0800

    第14题，第2解法，速度24ms左右，击败99.5%

---
+++ 14/2.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if len(strs) == 0:
+            a = ""
+            return a
+        a = strs[0]
+        for i in strs[1:]:
+            while i.find(a) != 0:
+                a = a[:-1]
+        return a
+
+
+
+a = Solution().longestCommonPrefix(["c","acc","ccc"])
+print(a)
