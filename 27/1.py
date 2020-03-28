0016fab516024d00353117f1bab94e78fc11a31a
xilongxin <xilongxin@weigankeji.com>
2020-03-28 19:22:02 +0800

    第27题，第1解法，速度44ms左右，击败32%

--- 27/1.py
+++ 27/1.py
@@ -1,9 +1,10 @@
 from typing import List
 
+
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         i = 0
-        while i <= len(nums) -1:
+        while i <= len(nums) - 1:
             if nums[i] == val:
                 del nums[i]
             else:
