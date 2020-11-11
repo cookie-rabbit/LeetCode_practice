62faf5282a099078992f9d43ab74315b3742c281
xilongxin <xilongxin@weigankeji.com>
2020-11-11 22:27:01 +0800

    二叉树中序和前序遍历写反了，调整回来

--- 二叉树/94 二叉树中序遍历/1.py
+++ 二叉树/94 二叉树中序遍历/1.py
@@ -3,15 +3,17 @@
 from typing import List
 from 二叉树.tree_node import TreeNode
 
+
 class Solution:
-    def preorderTraversal(self, root: TreeNode) -> List[int]:
+    def midorderTraversal(self, root: TreeNode) -> List[int]:
         ss = []
-        def preorder(root: TreeNode):
+
+        def midorder(root: TreeNode):
             if not root:
                 return root
+            midorder(root.left)
             ss.append(root.val)
-            preorder(root.left)
-            preorder(root.right)
+            midorder(root.right)
 
-        preorder(root)
+        midorder(root)
         return ss
