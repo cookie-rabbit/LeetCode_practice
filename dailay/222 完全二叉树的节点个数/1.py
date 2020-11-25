0124b8b1ec93a886ce230449449d9479a2441606
Noirey <862151891@qq.com>
2020-11-25 22:13:50 +0800

    222

--- dailay/222 完全二叉树的节点个数/1.py
+++ dailay/222 完全二叉树的节点个数/1.py
@@ -17,7 +17,7 @@
 # 输出: 6
 from 二叉树.tree_node import TreeNode
 
-# 只问节点数，数出所有非叶节点的节点就可以了
+# 只问节点数，数出所有非叶节点的节点就可以了(但这种方法效率低)
 class Solution:
     def countNodes(self, root: TreeNode) -> int:
         if not root:
