bb6ac4cf058b76a9f68dd74765cc719bbd5d7fe8
xilongxin <xilongxin@weigankeji.com>
2020-11-26 20:38:32 +0800

    164

---
+++ dailay/164 最大间距/1.py
@@ -0,0 +1,36 @@
+# https://leetcode-cn.com/problems/maximum-gap/
+# 给定一个无序的数组，找出数组在排序之后，相邻元素之间最大的差值。
+#
+# 如果数组元素个数小于 2，则返回 0。
+#
+# 示例1:
+# 输入: [3,6,9,1]
+# 输出: 3
+# 解释: 排序后的数组是 [1,3,6,9], 其中相邻元素 (3,6) 和 (6,9) 之间都存在最大差值 3。
+#
+# 示例2:
+# 输入: [10]
+# 输出: 0
+# 解释: 数组元素个数小于 2，因此返回 0。
+# 说明:
+#
+# 你可以假设数组中所有元素都是非负整数，且数值在 32 位有符号整数范围内。
+# 请尝试在线性时间复杂度和空间复杂度的条件下解决此问题。
+from typing import List
+
+# 笨方法，排序后直接数一遍
+class Solution:
+    def maximumGap(self, nums: List[int]) -> int:
+        if not nums or len(nums) < 2:
+            return 0
+
+        slide = 0
+        nums.sort()
+        i = 0
+        while i < len(nums) - 1:
+            ans = nums[i + 1] - nums[i]
+            if ans > slide:
+                slide = ans
+            i += 1
+
+        return slide
