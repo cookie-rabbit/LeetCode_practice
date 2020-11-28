cee19f5ad7dc6c68c883c6eb7cc998f97b9b2f09
xilongxin <xilongxin@weigankeji.com>
2020-11-28 16:29:40 +0800

    116

---
+++ 二叉树/116 填充每个节点的下一个右侧节点指针/1.py
@@ -0,0 +1,80 @@
+# https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node/
+# 给定一个完美二叉树，其所有叶子节点都在同一层，每个父节点都有两个子节点。二叉树定义如下：
+#
+# struct Node {
+#   int val;
+#   Node *left;
+#   Node *right;
+#   Node *next;
+# }
+# 填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+#
+# 初始状态下，所有 next 指针都被设置为 NULL。
+import collections
+
+from 二叉树.tree_node import TreeNode
+
+
+class Solution:
+    def connect(self, root: TreeNode) -> TreeNode:
+        if not root:
+            return root
+
+        # 初始化队列同时将第一层节点加入队列中，即根节点
+        Q = collections.deque([root])
+
+        # 外层的 while 循环迭代的是层数
+        while Q:
+
+            # 记录当前队列大小
+            size = len(Q)
+
+            # 遍历这一层的所有节点
+            for i in range(size):
+
+                # 从队首取出元素
+                node = Q.popleft()
+
+                # 连接
+                if i < size - 1:
+                    node.next = Q[0]
+
+                # 拓展下一层节点
+                if node.left:
+                    Q.append(node.left)
+                if node.right:
+                    Q.append(node.right)
+
+        # 返回根节点
+        return root
+
+
+class Solution2:
+    def connect(self, root: TreeNode) -> TreeNode:
+
+        if not root:
+            return root
+
+        # 从根节点开始
+        leftmost = root
+
+        while leftmost.left:
+
+            # 遍历这一层节点组织成的链表，为下一层的节点更新 next 指针
+            head = leftmost
+            while head:
+
+                # CONNECTION 1
+                head.left.next = head.right
+
+                # CONNECTION 2
+                if head.next:
+                    head.right.next = head.next.left
+
+                # 指针向后移动
+                head = head.next
+
+            # 去下一层的最左的节点
+            leftmost = leftmost.left
+
+        return root
