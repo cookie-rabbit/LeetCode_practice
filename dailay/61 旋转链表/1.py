f5ad37b691512599f86fe4a77f1b34b04c1fd777
Noirey <862151891@qq.com>
2021-03-27 22:43:00 +0800

    61

--- dailay/61 旋转链表/1.py
+++ dailay/61 旋转链表/1.py
@@ -1,4 +1,18 @@
+# https://leetcode-cn.com/problems/rotate-list/
+# 给你一个链表的头节点 head ，旋转链表，将链表每个节点向右移动 k 个位置，求新链表。
+# 输入：head = [1,2,3,4,5], k = 2
+# 输出：[4,5,1,2,3]
+#
+# 输入：head = [0,1,2], k = 4
+# 输出：[2,0,1]
+#
+# 提示：
+# 链表中节点的数目在范围 [0, 500] 内
+# -100 <= Node.val <= 100
+# 0 <= k <= 2 * 109
+
 class Solution:
+    # 向后走了 k 位，则新链表的头一定是 -k，简单的想就是 -k + k = 0，说明 -k 就是新链表的头，当然，k要取模，也就是去除多余的循环。
     def rotateRight(self, head, k):
         if not head or not head.next:
             return head
@@ -19,6 +33,9 @@ class Solution:
             k -= 1
         # 此时 slow 和 fast 之间的距离是 k；fast 指向第 k+1 个节点
         # 当 fast.next 为空时，fast 指向链表最后一个节点，slow 指向倒数第 k + 1 个节点
+
+        # 上面这么做的目的是找出 -k，方法是先拉开 k 的距离，然后当 fast 指向最后一位时，slow 因为距离 fast 的距离为 k，
+        # 所以 slow 指向 k+1，则 slow.next 指向的就是 -k，也就是新链表的头
         while fast.next:
             fast = fast.next
             slow = slow.next
